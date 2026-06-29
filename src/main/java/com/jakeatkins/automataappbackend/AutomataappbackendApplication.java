a79bb507f295977194df6982e4726f11d40b9941
Jake Atkins <atkins.jake@icloud.com>
2026-06-29 12:08:32 +0100

    init commit

---
+++ src/main/java/com/jakeatkins/automataappbackend/AutomataappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.jakeatkins.automataappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomataappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomataappbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakeatkins/automataappbackend/AutomataappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakeatkins.automataappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomataappbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
