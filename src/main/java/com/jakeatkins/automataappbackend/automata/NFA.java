81b2505f458aa1dc9f56533d91cec49c3a649c16
Jake Atkins <atkins.jake@icloud.com>
2026-06-30 11:05:39 +0100

    created classes for NFA and DFA, the stores states, acceptingstates, alpahbet, start state, transitions and state labels and all their equivalent getters, using Set<Integer> to store states

---
+++ src/main/java/com/jakeatkins/automataappbackend/automata/NFA.java
@@ -0,0 +1,66 @@
+package com.jakeatkins.automataappbackend.automata;
+
+import java.util.Map;
+import java.util.Set;
+
+public class NFA implements Automata<Integer>{
+    
+    private final Set<Integer> states;
+    private final Set<Integer> acceptingStates;
+    private final Set<Character> alphabet;
+    private final Integer startState;
+    private final Map<Integer, Map<Character,Set<Integer>>> transitionMap;
+    private final Map<Integer,String> stateLabelMap;
+
+    public NFA(
+        Integer startState, 
+        Set<Integer> states, 
+        Set<Integer> acceptingStates, 
+        Set<Character> alphabet,
+        Map<Integer, Map<Character,Set<Integer>>> transitionMap,
+        Map<Integer,String> stateLabelMap
+    ){
+        this.startState = startState;
+        this.states = states;
+        this.acceptingStates = acceptingStates;
+        this.alphabet = alphabet;
+        this.transitionMap = transitionMap;
+        this.stateLabelMap = stateLabelMap;
+    }
+
+    @Override
+    public Set<Character> getAlphabet() {
+        return alphabet;
+    }
+
+    @Override
+    public Integer getStartState() {
+        return startState;
+    }
+
+    @Override
+    public Set<Integer> getAcceptingStates() {
+        return acceptingStates;
+    }
+
+    @Override
+    public Set<Integer> getStates() {
+        return states;
+    }
+
+    @Override
+    public Map<Integer, Map<Character, Set<Integer>>> getTransitionMap() {
+        return transitionMap;
+    }
+
+    @Override
+    public Map<Integer, String> getStateLabelMap() {
+        return stateLabelMap;
+    }
+
+
+
+
+    
+
+}
