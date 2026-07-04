9d43d043dfe26d54c80824e40dd55736cd3f26d4
Jake Atkins <atkins.jake@icloud.com>
2026-07-04 16:01:54 +0100

    Added NfaToDfaConverter method convert which is the helper to load in a GlobalStateIdGenerator to the subsetConstruction. Added the subsetConstruction method that takes in an NFA and an Id generator and converts and NFA to a DFA keeping all state information. The subsetConstruction first gets the epsilon closure of the starting state of the input NFA then adds that state to the unmarkedStates.  In order to correctly map state Labels from the NFA into the new DFA (as DFA can have a set of states forming one state, after conversion) I used a Map to map sets of states to new Ids of state generated by the GlobalStateIdGenerator, e.g. create a new state ID for the DFA as q0 for the starting state, then map it to label of a set of states derived from the epsilon closure of the NFA. After this I check that the starting set of states of the DFA contains any state in the accepting states of the NFA, if the sets overlap for any state then the set must be an accepting state and be added to the DFA accepting states. Then, in order to build the transitionMap for the new DFA, pop the stack of the unmarked states and for every character in the NFA alphabet, cycle through all the states in the current set of states, and look it up in the NFA transitionMap. Once a set is found for that symbol it can be passed into the epsilon closure. After getting the epsilon closure I check if the DFA multipleStatesToId map contains this generated epsilon closure set of states, if it doesn’t, then generate a new state ID and add it to the map along with the set of states, and push that set to unmarkedStates. Once again check if that set contains an accepting state, and if so add it to the DFA accepting states. Finally add the new Id,Symbol and Set of States to the TransitionMap of the new DFA but checking if the ID and symbol are present, then adding the new state. This basically creates 2 different state IDs for the new DFA (internal is a list of new states 0,1,2) which map to external state labels, stored in a map so that when converting from NFA to DFA no state info is lost (q0 could be mapped to {q0q1} etc). Also added the subsetLabelGenerator to nest the states derived from the NFA (e.g. if a NFA has states q1 and {q2,q3}, and they get merged in a conversion it returns a nest label of {q1,{q2,q3}}.

--- src/main/java/com/jakeatkins/automataappbackend/algos/NfaToDfaConverter.java
+++ src/main/java/com/jakeatkins/automataappbackend/algos/NfaToDfaConverter.java
@@ -1,10 +1,13 @@
 package com.jakeatkins.automataappbackend.algos;
 
+import java.util.ArrayDeque;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.jakeatkins.automataappbackend.automata.DFA;
 import com.jakeatkins.automataappbackend.automata.NFA;
@@ -25,34 +28,84 @@ public class NfaToDfaConverter {
         Integer startState = idGen.next(); //internal state should be 0, but extenernal should be epsilon closure of NFA states
         Set<Integer> states = new HashSet<>();
         Set<Integer> acceptingStates = new HashSet<>();
-        Set<Character> alphabet = new HashSet<>();
+        
+        Set<Character> alphabet = new HashSet<>(nfa.getAlphabet());
+        alphabet.remove(EPSILON);
+
         Map<Integer,Map<Character,Set<Integer>>> transitionMap = new HashMap<>();
+
         Map<Integer,String> stateLabelMap = new HashMap<>();
+        Map<Set<Integer>,Integer> multipleStatesToSingleDfaIdMap = new HashMap<>();
         
         Set<Integer> dfaStartingStateSet = EpsilonClosure.epsilonClosure(Set.of(nfa.getStartState()), nfa.getTransitionMap());
 
+        if(!Collections.disjoint(dfaStartingStateSet, nfa.getAcceptingStates())){
+            acceptingStates.add(startState);
+        }
 
+        multipleStatesToSingleDfaIdMap.put(dfaStartingStateSet,startState); 
+        states.add(startState);
 
-        
-        
-        
-        
-        
-        
-        
-        
+        stateLabelMap.put(startState,subsetLabelGenerator(dfaStartingStateSet,nfa.getStateLabelMap())); 
+
+        Deque<Set<Integer>> unmarkedStates = new ArrayDeque<>();
+
+        unmarkedStates.add(dfaStartingStateSet);
+
+        while(!unmarkedStates.isEmpty()){
+
+            Set<Integer> currentSetStates = unmarkedStates.pop();
+            Integer currentId = multipleStatesToSingleDfaIdMap.get(currentSetStates);
+            
+            for(Character symbol: alphabet){
+                Set<Integer> tempSet = new HashSet<>();
+
+                for(Integer state : currentSetStates){
+
+                    Set<Integer> targetStates = nfaTransitionLookup(state, symbol, nfa.getTransitionMap());
+                    tempSet.addAll(targetStates);
+                }
+                
+                Set<Integer> nextSet = EpsilonClosure.epsilonClosure(tempSet, nfa.getTransitionMap());
+
+                if(!multipleStatesToSingleDfaIdMap.containsKey(nextSet)){
+                    Integer nextId = idGen.next();
+                    multipleStatesToSingleDfaIdMap.put(nextSet, nextId);
+                    states.add(nextId);
+                    unmarkedStates.push(nextSet);
+
+                    stateLabelMap.put(nextId,subsetLabelGenerator(nextSet,nfa.getStateLabelMap())); 
+                    if(!Collections.disjoint(nextSet, nfa.getAcceptingStates())){
+                    acceptingStates.add(nextId);
+                    }
+                }
+
+                Integer targetId = multipleStatesToSingleDfaIdMap.get(nextSet);
+
+                transitionMap.computeIfAbsent(currentId, r -> new HashMap<>()).computeIfAbsent(symbol,r-> new HashSet<>()).add(targetId);
+            }
+         }  
         return new DFA(startState,states,acceptingStates,alphabet,transitionMap,stateLabelMap);
     }
 
-
-    public static Set<Integer> nfaTransitionLookup(int state, char symbol, Map<Integer, Map<Character, Set<Integer>>> nfaTransitionMap){
+    private static Set<Integer> nfaTransitionLookup(int state, char symbol, Map<Integer, Map<Character, Set<Integer>>> nfaTransitionMap){
         if(!nfaTransitionMap.containsKey(state) ||!nfaTransitionMap.get(state).containsKey(symbol)){
             return Collections.emptySet();
         }
-
         return nfaTransitionMap.get(state).get(symbol);
     }
 
+    private static String subsetLabelGenerator(Set<Integer> subset, Map<Integer,String> nfaTransitionMap){
+        if(subset.isEmpty()){return "∅";}
 
-
+        return subset.stream().sorted()
+        .map(r -> {
+            String label = nfaTransitionMap.get(r);
+            if(label == null ||label.isBlank()){
+                return "q" + r;
+            }
+            return label;
+        })
+        .collect(Collectors.joining(",","{","}"));
+    }
 }
