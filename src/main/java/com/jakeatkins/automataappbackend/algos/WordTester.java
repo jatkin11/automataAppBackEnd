6a19a9474149df5041b5b0d18f6359f8db1b534a
Jake Atkins <atkins.jake@icloud.com>
2026-07-07 16:47:18 +0100

    Added WordTest that takes an Automata or Regex String, and tests a users word to see if the word is accepted by the language of the automata/regex. To achieve this  I use the transition map from the automata, and the starting states. I get the epsilon closure from the starting states, then for each symbol of the word to test, update the list of states with the epsilon closure of reachable states of that symbol. Eventually once all symbols have been checked the final list of integers will be the states reachable by the entered word. If any of those states are also accepting states then the word is accepted. To test just a string, I use a helper method that converts the string into an NFA then test that NFA and user word.

---
+++ src/main/java/com/jakeatkins/automataappbackend/algos/WordTester.java
@@ -0,0 +1,54 @@
+package com.jakeatkins.automataappbackend.algos;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.jakeatkins.automataappbackend.automata.Automata;
+import com.jakeatkins.automataappbackend.automata.NFA;
+import com.jakeatkins.automataappbackend.regex.RegexToken;
+
+public class WordTester {
+
+public static boolean testAutomata(Automata automata, String word){
+    if (automata == null || word == null){
+        return false;
+    }
+
+    Map<Integer,Map<Character,Set<Integer>>> transitionMap = automata.getTransitionMap();
+
+    Set<Integer> startStates = new HashSet<>();
+    startStates.add(automata.getStartState());
+
+    Set<Integer> states = new HashSet<>(EpsilonClosure.epsilonClosure(startStates, transitionMap));
+    
+    for(Character symbol : word.toCharArray()){
+        Set<Integer> nextStates = new HashSet<>();
+
+        for(Integer state: states){
+            Set<Integer> reachableStates = automata.getTransitionMap().getOrDefault(state,Collections.emptyMap()).getOrDefault(symbol,Collections.emptySet());
+            nextStates.addAll(reachableStates);
+        }
+
+        states = EpsilonClosure.epsilonClosure(nextStates, automata.getTransitionMap());
+    }
+
+    for(Integer state : states){
+        if(automata.getAcceptingStates().contains(state)){
+            return true;
+        }
+    }
+    return false;
+}
+
+    public static boolean testRegexString(String regex, String word){
+        if(regex == null || word == null){
+            return false;
+        }
+        RegexToken rt = new RegexTokeniser(regex).tokenise();
+        NFA nfa = new RegexToNfaConverter(rt).convert();
+        return testAutomata(nfa, word);
+    }
+    
+}
