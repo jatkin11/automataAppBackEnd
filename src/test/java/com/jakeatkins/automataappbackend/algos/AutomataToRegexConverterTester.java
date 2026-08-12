2262ef48243575c79e4ab589688aa2c905b6fbbe
Jake Atkins <atkins.jake@icloud.com>
2026-08-12 21:44:49 +0100

    added additional junit tests to the AutomataToRegexConverterTester

--- src/test/java/com/jakeatkins/automataappbackend/algos/AutomataToRegexConverterTester.java
+++ src/test/java/com/jakeatkins/automataappbackend/algos/AutomataToRegexConverterTester.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.Test;
 
 import com.jakeatkins.automataappbackend.regex.RegexConcat;
 import com.jakeatkins.automataappbackend.regex.RegexEmptySet;
+import com.jakeatkins.automataappbackend.regex.RegexStarred;
 import com.jakeatkins.automataappbackend.regex.RegexSymbol;
 import com.jakeatkins.automataappbackend.regex.RegexUnion;
 import static com.jakeatkins.automataappbackend.utilities.TestAutomataCreator.nfaEmptyWord;
 import static com.jakeatkins.automataappbackend.utilities.TestAutomataCreator.nfaSingleA;
 import static com.jakeatkins.automataappbackend.utilities.TestAutomataCreator.nfaSingleAWithNoAcceptingState;
 import static com.jakeatkins.automataappbackend.utilities.TestAutomataCreator.nfaWithConcatAB;
+import static com.jakeatkins.automataappbackend.utilities.TestAutomataCreator.nfaWithMultipleBranches;
+import static com.jakeatkins.automataappbackend.utilities.TestAutomataCreator.nfaWithSelfLoop;
+import static com.jakeatkins.automataappbackend.utilities.TestAutomataCreator.nfaWithTwoEpsilonJumpThenA;
 import static com.jakeatkins.automataappbackend.utilities.TestAutomataCreator.nfaWithUnionAorB;
 
 public class AutomataToRegexConverterTester {
@@ -31,10 +35,10 @@ public class AutomataToRegexConverterTester {
         assertEquals(new RegexUnion(new RegexSymbol('a', -1), new RegexSymbol('b',-1)), new AutomataToRegexConverter(nfaWithUnionAorB()).convert());
     }
 
-    //  @Test
-    // void convertsStarredContainingNFA(){
-
-    // }
+    @Test
+     void convertsStarredContainingNFA(){
+        assertEquals(new RegexStarred(new RegexSymbol('a',-1)),new AutomataToRegexConverter(nfaWithSelfLoop()).convert());
+    }
 
      @Test
     void convertsNFAofEmptyWord(){
@@ -46,15 +50,15 @@ public class AutomataToRegexConverterTester {
         assertEquals(new RegexEmptySet(), new AutomataToRegexConverter(nfaSingleAWithNoAcceptingState()).convert());
     }
 
-    //  @Test
-    // void convertsDFAWithMultipleAcceptingStates(){
+     @Test
+    void convertsDFAWithMultipleAcceptingStates(){
+        assertEquals(new RegexSymbol('a',-1), new AutomataToRegexConverter(nfaWithMultipleBranches()).convert());
+    }
 
-    // }
-
-    //  @Test
-    // void convertsNFAWithEpsilons(){
-
-    // }
+    @Test
+    void convertsNFAWithEpsilons(){
+        assertEquals(new RegexSymbol('a',-1), new AutomataToRegexConverter(nfaWithTwoEpsilonJumpThenA()).convert());
+    }
 
  
 
