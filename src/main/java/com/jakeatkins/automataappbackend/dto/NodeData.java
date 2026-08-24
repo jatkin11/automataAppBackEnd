118c323d8b8d9d78a038de1d59b5372fec341c8f
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:51:48 +0100

    added JavaDocs to NodeData

--- src/main/java/com/jakeatkins/automataappbackend/dto/NodeData.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/NodeData.java
@@ -1,4 +1,16 @@
 package com.jakeatkins.automataappbackend.dto;
 
+/**
+ * 
+ * NodeData
+ * 
+ * DTO containing the custom data required for Automata represention by a ReactFlowNode
+ * 
+ * NodeData is encapsulated within each node
+ * 
+ * @param label string of front-end node display label
+ * @param startingState boolean of node being a starting state
+ * @param acceptingState boolean of node being an accepting state
+ */ 
 
 public record NodeData (String label, boolean startingState, boolean acceptingState){}
