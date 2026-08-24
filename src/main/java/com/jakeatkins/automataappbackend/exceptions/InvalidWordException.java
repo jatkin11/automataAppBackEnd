b19adbb67c48092669a3f8c475971aef9b3da1f1
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:19:05 +0100

    added JavaDocs to InvalidWordException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidWordException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidWordException.java
@@ -1,5 +1,12 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * 
+ * InvalidWordException
+ * 
+ *  Custom exception for Invalid Word DTO
+ */
+
 public class InvalidWordException extends RuntimeException{
     
     public InvalidWordException(String message){
