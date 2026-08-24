fc97499af3165d5d26a087109868b7a0ff5d10a8
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:29:56 +0100

    added JavaDocs to RegexEpsilon

--- src/main/java/com/jakeatkins/automataappbackend/regex/RegexEpsilon.java
+++ src/main/java/com/jakeatkins/automataappbackend/regex/RegexEpsilon.java
@@ -1,3 +1,14 @@
 package com.jakeatkins.automataappbackend.regex;
 
+/**
+ * 
+ * RegexEpsilon
+ * 
+ *  Record of Epsilon RegexToken
+ * 
+ *  Implements RegexToken Interface
+ * 
+ *  Represents an epsilon 'ε'
+ */
+
 public record RegexEpsilon() implements RegexToken{}
