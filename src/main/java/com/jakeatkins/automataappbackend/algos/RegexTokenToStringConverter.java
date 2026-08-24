51f7a7c89ab3e6e1c4299d847a67febb9de6b798
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 17:36:42 +0100

    fixed RegexTokenToStringConverter to use the centralised AutomataSymbols instead of literalls. Added JavaDocs to RegexTokenToStringConverter

--- src/main/java/com/jakeatkins/automataappbackend/algos/RegexTokenToStringConverter.java
+++ src/main/java/com/jakeatkins/automataappbackend/algos/RegexTokenToStringConverter.java
@@ -1,13 +1,40 @@
 package com.jakeatkins.automataappbackend.algos;
 
-import com.jakeatkins.automataappbackend.exceptions.InvalidRegexTokenException;
-import com.jakeatkins.automataappbackend.regex.*;
-import com.jakeatkins.automataappbackend.exceptions.*;
-import static com.jakeatkins.automataappbackend.automata.AutomataSymbols.EPSILON;
+import static com.jakeatkins.automataappbackend.automata.AutomataSymbols.CLOSED_BRACKET;
 import static com.jakeatkins.automataappbackend.automata.AutomataSymbols.EMPTY_SET;
+import static com.jakeatkins.automataappbackend.automata.AutomataSymbols.EPSILON;
+import static com.jakeatkins.automataappbackend.automata.AutomataSymbols.OPEN_BRACKET;
+import static com.jakeatkins.automataappbackend.automata.AutomataSymbols.STAR;
+import static com.jakeatkins.automataappbackend.automata.AutomataSymbols.UNION;
+import com.jakeatkins.automataappbackend.exceptions.InvalidRegexException;
+import com.jakeatkins.automataappbackend.exceptions.InvalidRegexTokenException;
+import com.jakeatkins.automataappbackend.regex.RegexConcat;
+import com.jakeatkins.automataappbackend.regex.RegexEmptySet;
+import com.jakeatkins.automataappbackend.regex.RegexEpsilon;
+import com.jakeatkins.automataappbackend.regex.RegexStarred;
+import com.jakeatkins.automataappbackend.regex.RegexSymbol;
+import com.jakeatkins.automataappbackend.regex.RegexToken;
+import com.jakeatkins.automataappbackend.regex.RegexUnion;
 
+/**
+ * 
+ * RegexTokenToStringConverter
+ * 
+ * Converts RegexToken trees into regex strings e.g. RegexUnion(RegexSymbol('a'),RegexSymbol('b')) would produce: '(a|b)'
+ * 
+ */
 public class RegexTokenToStringConverter {
     
+    /**
+     * Converts RegexToken tree into string form regex
+     * 
+     * recursively traverses the tree to build regex string
+     * 
+     * @param regex RegexToken tree
+     * @return String of regex
+     * @throws InvalidRegexTokenException if RegexToken tree is null
+     * @throws InvalidRegexException if regex is invalid
+     */
     public static String convert(RegexToken regex){
         if(regex == null){
             throw new InvalidRegexTokenException("RegexToken cannot be null");
@@ -17,9 +44,9 @@ public class RegexTokenToStringConverter {
             case RegexSymbol r-> String.valueOf(r.symbol());
             case RegexEpsilon r -> String.valueOf(EPSILON);
             case RegexEmptySet r-> String.valueOf(EMPTY_SET);
-            case RegexStarred r-> "(" + convert(r.starredRegex()) + ")*";
-            case RegexConcat r-> "(" + convert(r.left()) + convert(r.right()) + ")";
-            case RegexUnion r-> "(" + convert(r.left()) + "|" + convert(r.right()) + ")";
+            case RegexStarred r-> OPEN_BRACKET + convert(r.starredRegex()) + CLOSED_BRACKET + STAR;
+            case RegexConcat r-> OPEN_BRACKET + convert(r.left()) + convert(r.right()) + CLOSED_BRACKET;
+            case RegexUnion r-> OPEN_BRACKET + convert(r.left()) + UNION + convert(r.right()) + CLOSED_BRACKET;
             default -> throw new InvalidRegexException("Invalid regexToken tree");};
     }
 }
