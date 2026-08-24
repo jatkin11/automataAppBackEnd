492df75513705ca32191f64b4fac26797f3a5587
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:36:39 +0100

    added JavaDocs to RegexUnion

--- src/main/java/com/jakeatkins/automataappbackend/regex/RegexUnion.java
+++ src/main/java/com/jakeatkins/automataappbackend/regex/RegexUnion.java
@@ -1,3 +1,18 @@
 package com.jakeatkins.automataappbackend.regex;
 
+/**
+ * 
+ * RegexUnion
+ * 
+ * Record of Union RegexToken
+ * 
+ * Implements RegexToken Interface
+ * 
+ * @param left contains the left regexToken of the union of a regex
+ * @param right contains the right regexToken of the union of a regex
+ * 
+ * Represents a union in a regex e.g. 'a|b' where 'a' and 'b' are RegexSymbols
+ * 
+ */
+
 public record  RegexUnion (RegexToken left, RegexToken right) implements RegexToken{}
