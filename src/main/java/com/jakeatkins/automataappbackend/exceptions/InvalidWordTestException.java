9983c851f2bdbee087c31cfc6434a7faf494f30e
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:17:43 +0100

    added JavaDocs to InvalidWordTestException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidWordTestException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidWordTestException.java
@@ -1,5 +1,12 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * InvalidWordTestException
+ * 
+ * Custom exception for Invalid WordTest Data Transfer Object
+ * 
+ */
+
 public class InvalidWordTestException extends RuntimeException {
     
     public InvalidWordTestException(String message){
