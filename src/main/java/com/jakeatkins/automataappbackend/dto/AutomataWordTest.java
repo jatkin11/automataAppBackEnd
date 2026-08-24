a8feadf3b438f89a92a14a91717a79ec7e2dda08
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:48:02 +0100

    added JavaDocs to AutomataWord test dto

--- src/main/java/com/jakeatkins/automataappbackend/dto/AutomataWordTest.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/AutomataWordTest.java
@@ -1,3 +1,14 @@
 package com.jakeatkins.automataappbackend.dto;
 
+/**
+ * 
+ * AutomataWordTest
+ * 
+ * DTO containing a ReactFlowGraph representation of an Automaton and a word to test
+ * 
+ * @param graph ReactFlowGraph of Automaton
+ * @param word string of word to test on Automaton
+ * 
+ */
+
 public record  AutomataWordTest(ReactFlowGraph graph, String word){}
