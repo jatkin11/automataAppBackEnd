a28410da8ab77d454c2c78e6747af8895989bc10
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:28:40 +0100

    added JavaDocs to RegexStringValidator

--- src/main/java/com/jakeatkins/automataappbackend/validators/RegexStringValidator.java
+++ src/main/java/com/jakeatkins/automataappbackend/validators/RegexStringValidator.java
@@ -1,10 +1,26 @@
 package com.jakeatkins.automataappbackend.validators;
 
-import com.jakeatkins.automataappbackend.dto.*;
-import com.jakeatkins.automataappbackend.exceptions.*;
+import com.jakeatkins.automataappbackend.dto.RegexString;
+import com.jakeatkins.automataappbackend.exceptions.InvalidRegexStringException;
+
+/**
+ * 
+ * RegexStringValidator
+ * 
+ * Validation class for RegexString DTO
+ * 
+ */
 
 public class RegexStringValidator {
     
+    /**
+     * Checks:
+     * - Passed RegexString is not null
+     * - composite regex string is not null
+     * 
+     * @param regex RegexString DTO of regex
+     * @throws InvalidRegexStringException if any validation fails
+     */
     public static void validate(RegexString regex){
         if(regex == null){
             throw new InvalidRegexStringException("RegexString cannot be null");
