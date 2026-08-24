c78bad8fd307890cb4b4c0ad03fb1891500971ba
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 16:48:03 +0100

    added JavaDocs to UniqueStateGenerator

--- src/main/java/com/jakeatkins/automataappbackend/algos/UniqueStateGenerator.java
+++ src/main/java/com/jakeatkins/automataappbackend/algos/UniqueStateGenerator.java
@@ -3,8 +3,24 @@ package com.jakeatkins.automataappbackend.algos;
 import java.util.Objects;
 import java.util.Set;
 
+/**
+ * 
+ * UniqueStateGenerator
+ * 
+ * Generates a new state ID given a current set of state IDs
+ * 
+ * Returns next available positive int starting from 0 not contained in the set.
+ * 
+ */
+
 public class UniqueStateGenerator {
     
+    /**
+     * Generates next positive int from 0
+     * 
+     * @param states set of states IDs of an automaton
+     * @return next available int
+     */
     public static int generate(Set<Integer> states){
         Objects.requireNonNull(states);
 
