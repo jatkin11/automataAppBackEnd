9e8f7642afb51783cb1d5deb3db074f839d97eb1
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:21:10 +0100

    added JavaDocs to InvalidRegexStringException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidRegexStringException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidRegexStringException.java
@@ -1,5 +1,12 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * 
+ * InvalidRegexStringException
+ * 
+ * Custom exception for Invalid RegexString DTO
+ */
+
 public class InvalidRegexStringException extends RuntimeException{
         
         public InvalidRegexStringException(String message){
