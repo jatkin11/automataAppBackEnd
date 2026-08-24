3e054820f281e85f2fa7992fecf3e77e0fad33f1
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:53:30 +0100

    added JavaDocs to Position

--- src/main/java/com/jakeatkins/automataappbackend/dto/Position.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/Position.java
@@ -1,3 +1,15 @@
 package com.jakeatkins.automataappbackend.dto;
 
+/**
+ * 
+ * Position
+ * 
+ * X and Y co-ords required by a ReactFlowNode
+ * 
+ * Position is encapsulated within a ReactFlowNode
+ *  
+ * @param x int of x-ord
+ * @param y int of y-ord
+ */
+
 public record Position (int x, int y){}
