44225052159da28cb9186258ef5e442e7282c145
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 17:20:38 +0100

    added JavaDocs to GlobalExceptionHandler

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,42 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
-import java.time.*;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import java.time.Instant;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 
+ * GlobalExceptionHandler
+ * 
+ * Global exception handling for all API requests
+ * 
+ * For known custom exception classes, custom exception turned into ApiException which is attached to HTTP 400 response:
+ * - "Bad Request"
+ * - the error message
+ * - HTTP status: BAD_REQUEST
+ * - timestamp
+ * 
+ * For unknown general exceptions, turned into ApiExpception which is attached to HTTP 500 response:
+ * - "Internal Server Error",
+ * - the error message
+ * - HTTP status: INTERNAL_SERVER_ERROR
+ * - timestamp
+ * 
+ */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Custom exception Handler for known exceptions
+     * 
+     * @param e the execption
+     * @return HTTP 400 resposne with ApiException body
+     */
     @ExceptionHandler({InvalidAutomataException.class,
-                        InvalidReactFlowGraphException.class, 
+                    InvalidReactFlowGraphException.class, 
                         InvalidRegexException.class,
                         InvalidRegexTokenException.class,
                         InvalidWordTestException.class,
@@ -22,6 +50,11 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(apiException);
     }
 
+    /**
+     * General unexpected exception handler (for non-custom exceptions) i.e. a catch-all
+     * @param e the exception
+     * @return HTTP 500 response with ApiException body
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiException> unexpectedException(Exception e){
         ApiException apiException = new ApiException("Internal Server Error", e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), Instant.now());
