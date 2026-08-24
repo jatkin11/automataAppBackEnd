58dd580b01f4ffbe2b3ae6b72d12299bb8c2a10a
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:57:42 +0100

    added JavaDocs to WordValidator

--- src/main/java/com/jakeatkins/automataappbackend/validators/WordValidator.java
+++ src/main/java/com/jakeatkins/automataappbackend/validators/WordValidator.java
@@ -1,9 +1,26 @@
 package com.jakeatkins.automataappbackend.validators;
 
-import com.jakeatkins.automataappbackend.exceptions.*;
+import com.jakeatkins.automataappbackend.exceptions.InvalidWordException;
+
+/**
+ * 
+ * WordValidator
+ * 
+ * Validation class of user-inputted word to test
+ * 
+ */
 
 public class WordValidator {
     
+    /**
+     * Checks:
+     * - word is not null
+     * - word chars are A-Z,a-z,0-9
+     * - allows empty string as it represents Automata langugage "ε"
+     * 
+     * @param word user-inputted word to test
+     * @throws InvalidWordException, if any validation fails
+     */
     public static void validate(String word){
         if(word == null){
             throw new InvalidWordException("Word cannot be null");
