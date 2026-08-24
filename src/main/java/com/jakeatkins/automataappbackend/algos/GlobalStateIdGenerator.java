1b658106464eb387460d2de28517167f340b5cbc
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 16:51:05 +0100

    added JavaDocs to GlobalStateIdGenerator

--- src/main/java/com/jakeatkins/automataappbackend/algos/GlobalStateIdGenerator.java
+++ src/main/java/com/jakeatkins/automataappbackend/algos/GlobalStateIdGenerator.java
@@ -1,9 +1,19 @@
 package com.jakeatkins.automataappbackend.algos;
 
+/**
+ * 
+ * GlobalStateIdGenerator
+ * 
+ * Generates a new ID starting at 0, non-static to provide sequential ints
+ */
 public class GlobalStateIdGenerator {
     
     private int nextId = 0;
 
+    /**
+     * Provides new state ID starting at 0 and incrementing every times its called
+     * @return next int
+     */
     public int next(){
         return nextId++;
     }
