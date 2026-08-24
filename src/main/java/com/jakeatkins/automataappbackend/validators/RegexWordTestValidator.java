a33d6ae2def81f278760064bc9b3b124a3a83320
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:30:29 +0100

    added JavaDocs to RegexWordTestValidator

--- src/main/java/com/jakeatkins/automataappbackend/validators/RegexWordTestValidator.java
+++ src/main/java/com/jakeatkins/automataappbackend/validators/RegexWordTestValidator.java
@@ -3,8 +3,24 @@ package com.jakeatkins.automataappbackend.validators;
 import com.jakeatkins.automataappbackend.dto.RegexWordTest;
 import com.jakeatkins.automataappbackend.exceptions.InvalidRegexWordTestException;
 
+/**
+ * 
+ * RegexWordTestValidator
+ * 
+ * Validation class for RegexWordTest DTO
+ * 
+ */
 public class RegexWordTestValidator {
     
+    /**
+     * Checks:
+     * - RegexWordTest passed is not null
+     * - composite regex string is not null
+     * - composite word string is not null
+     * 
+     * @param rwt
+     * @throws InvalidRegexWordTestException if any validation fails
+     */
     public static void validate(RegexWordTest rwt){
         if(rwt == null){
             throw new InvalidRegexWordTestException("RWT cannot be null");
