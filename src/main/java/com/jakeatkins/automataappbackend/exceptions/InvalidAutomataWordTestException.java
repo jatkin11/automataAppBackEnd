0c237e4d5ecadd5981fc289175ab1f42d4e2d5fc
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:23:18 +0100

    added JavaDocs to InvalidAutomataWordTestException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidAutomataWordTestException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidAutomataWordTestException.java
@@ -1,5 +1,12 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * 
+ * InvalidAutomataWordTestException
+ * 
+ * Custom exception for Invalid AutomataWordTest DTO
+ */
+
 public class InvalidAutomataWordTestException extends RuntimeException {
     
         public InvalidAutomataWordTestException(String message){
