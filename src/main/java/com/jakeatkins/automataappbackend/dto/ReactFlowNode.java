9ec523a0547766947346feaeb61b591097f51fff
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:02:04 +0100

    added JavaDocs to ReactFlowNode

--- src/main/java/com/jakeatkins/automataappbackend/dto/ReactFlowNode.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/ReactFlowNode.java
@@ -1,4 +1,18 @@
 package com.jakeatkins.automataappbackend.dto;
 
+/**
+ * 
+ * ReactFlowNode
+ * 
+ * DTO of a Node within a ReactFlowGraph
+ * 
+ * Represents an Automaton State
+ * 
+ * @param id Node ID
+ * @param position Encapsulated Position object contain x/y co-ords
+ * @param data Enscapsulated NodeData object containing custom node info
+ * @param type Custom Node type used for front-end processing
+ */
+
 
 public record ReactFlowNode (String id, Position position, NodeData data, String type) {}
