1254459af6e62c52ff1bd1850310cd4d9d4fbd79
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:28:57 +0100

    added JavaDocs to RegexEmptySet

--- src/main/java/com/jakeatkins/automataappbackend/regex/RegexEmptySet.java
+++ src/main/java/com/jakeatkins/automataappbackend/regex/RegexEmptySet.java
@@ -1,3 +1,16 @@
 package com.jakeatkins.automataappbackend.regex;
 
+/**
+ * 
+ * RegexEmptySet
+ * 
+ *  Record of Empty Set RegexToken
+ * 
+ *  Implements RegexToken Interface
+ * 
+ *  Represents the empty set '∅'
+ * 
+ */
+
+
 public record RegexEmptySet() implements RegexToken{}
