725c706c22bdd6e65e2a98e8a20ef95685b49ce8
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:21:49 +0100

    added JavaDocs to InvalidRegexException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidRegexException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidRegexException.java
@@ -1,5 +1,12 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * 
+ * InvalidRegexException
+ * 
+ * Custom exception for Invalid Regex DTO
+ */
+
 public class InvalidRegexException extends RuntimeException {
     
         public InvalidRegexException(String message){
