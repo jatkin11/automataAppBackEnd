121447805423b334b1ccba85eb8f837654a82da4
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:19:46 +0100

    added JavaDocs to InvalidRegexWordTestException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidRegexWordTestException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidRegexWordTestException.java
@@ -1,5 +1,12 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * 
+ * InvalidRegexWordTestException
+ * 
+ * Custom exception for Invalid Regex Word Test DTO
+ */
+
 public class InvalidRegexWordTestException extends RuntimeException {
         
     public InvalidRegexWordTestException(String message){
