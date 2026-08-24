b3bdadd11530467cf9068d31e67277172e5007c9
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:34:49 +0100

    added JavaDocs to RegexSymbol

--- src/main/java/com/jakeatkins/automataappbackend/regex/RegexSymbol.java
+++ src/main/java/com/jakeatkins/automataappbackend/regex/RegexSymbol.java
@@ -1,3 +1,17 @@
 package com.jakeatkins.automataappbackend.regex;
 
+
+/**
+ *  
+ * RegexSymbol
+ * 
+ * Record of a transition symbol RegexToken
+ * 
+ * @param symbol is the transtion symbol
+ * @param position is the transition symbols Glushkov position within a regex, e.g a regex of 'aba' would have positions {a,1}, {b,2}, {a,3}.
+ * 
+ * Represents a transition symbol e.g. 'a'
+ * 
+ */
+
 public record  RegexSymbol (char symbol, int position)implements RegexToken{}
