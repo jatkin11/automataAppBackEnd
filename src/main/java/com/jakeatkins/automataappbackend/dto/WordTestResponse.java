697c0c7a60c4b3508a7eb63cc01ad5e230169ccf
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:05:38 +0100

    added JavaDocs to WordTestResposne

--- src/main/java/com/jakeatkins/automataappbackend/dto/WordTestResponse.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/WordTestResponse.java
@@ -1,3 +1,13 @@
 package com.jakeatkins.automataappbackend.dto;
 
+/**
+ * 
+ * WordTestResponse
+ * 
+ * DTO of word-testing response for either a RegexWordTest or AutomataWordTest
+ * 
+ * @param accepted boolean result of word-test
+ */
+
+
 public record  WordTestResponse(boolean accepted) {}
