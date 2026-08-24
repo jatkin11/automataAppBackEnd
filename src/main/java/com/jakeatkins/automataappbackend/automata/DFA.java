6e28676fb58bf0cf0f8b16aa77a58725f104896c
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:35:11 +0100

    added JavaDocs to DFA

--- src/main/java/com/jakeatkins/automataappbackend/automata/DFA.java
+++ src/main/java/com/jakeatkins/automataappbackend/automata/DFA.java
@@ -3,6 +3,21 @@ package com.jakeatkins.automataappbackend.automata;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * 
+ * DFA
+ * 
+ * Represents the Back-end DFA (deterministic finite automaton)
+ * 
+ * Implements Automata Interface
+ * 
+ * @param startState the automatons starting state
+ * @param states the automatons set of states
+ * @param acceptingStates the automatons set of accepting states
+ * @param alphabet the automatons alphabet (set of char)
+ * @param transitionMap the transition map between source and target states within an automaton e.g. 0 - 'a' -> 1
+ * @param stateLabelMap a map of state IDs vs their label to be displayed in the front-end e.g. <0, "q0">
+ */
 public record DFA (Integer startState, 
         Set<Integer> states, 
         Set<Integer> acceptingStates, 
