3d2547fd3c6e303fb233447cbadc420bf11ea0f3
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:20:36 +0100

    added JavaDocs to InvalidRegexTokenException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidRegexTokenException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidRegexTokenException.java
@@ -1,5 +1,13 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * 
+ * InvalidRegexTokenException
+ * 
+ * Custom exception for Invalid RegexToken DTO
+ */
+
+
 public class InvalidRegexTokenException extends RuntimeException{
     
         public InvalidRegexTokenException(String message){
