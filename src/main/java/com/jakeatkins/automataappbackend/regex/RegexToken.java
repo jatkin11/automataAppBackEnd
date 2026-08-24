a3ded3213eff78448ae47e2708994d6807a81dbe
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:40:11 +0100

    added JavaDocs to RegexToken

--- src/main/java/com/jakeatkins/automataappbackend/regex/RegexToken.java
+++ src/main/java/com/jakeatkins/automataappbackend/regex/RegexToken.java
@@ -1,3 +1,13 @@
 package com.jakeatkins.automataappbackend.regex;
 
+/**
+ * 
+ * RegexToken
+ * 
+ * Shared interface used by all the concrete regex token types. e.g. RegexSymbol etc.
+ * 
+ * Represents a token from a parsed regex e.g. 'a' or 'a|b'. A parsed regex will ultimatley form a RegexToken Tree with nested RegexToken.
+ * 
+ */
+
 public interface RegexToken {}
