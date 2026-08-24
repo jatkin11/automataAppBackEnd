ae67b07cc74cda6637d8d870cb926f0ba5904da0
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:31:58 +0100

    added JavaDocs to RegexStarred

--- src/main/java/com/jakeatkins/automataappbackend/regex/RegexStarred.java
+++ src/main/java/com/jakeatkins/automataappbackend/regex/RegexStarred.java
@@ -1,3 +1,16 @@
 package com.jakeatkins.automataappbackend.regex;
 
+/**
+ * 
+ * RegexStarred
+ * 
+ * Record of Kleene Star RegexToken
+ * 
+ * Implements RegexToken Interface
+ * 
+ * @param starredRegex contains the RegexToken to be starred
+ * 
+ * Represents a Kleene Star i.e. 'a*'' where 'a' is a RegexToken
+ */
+
 public record RegexStarred (RegexToken starredRegex)implements RegexToken{}
