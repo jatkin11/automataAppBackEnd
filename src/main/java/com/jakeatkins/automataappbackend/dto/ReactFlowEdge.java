00929f95ecaa1a00bc23732b09e15f49c15da2f2
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:56:46 +0100

    added JavaDocs to ReactFlowEdge

--- src/main/java/com/jakeatkins/automataappbackend/dto/ReactFlowEdge.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/ReactFlowEdge.java
@@ -1,3 +1,20 @@
 package com.jakeatkins.automataappbackend.dto;
 
+/**
+ * 
+ * ReactFlowEdge
+ * 
+ * DTO for an Edge within a ReactFlowGraph
+ * 
+ * Represents a transition between two states e.g. 0 - 'a' - > 1
+ * 
+ * @param id Edge ID
+ * @param source Node ID of source state
+ * @param target Node ID of target state
+ * @param label Transition label of edge
+ * @param type Custom edge type used for front-end processing
+ */
+
+
+
 public record ReactFlowEdge (String id, String source, String target, String label, String type){}
