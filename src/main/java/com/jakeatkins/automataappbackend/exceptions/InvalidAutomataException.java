aac0820244bdafb8a386dfc61d4bd592477e5749
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:24:11 +0100

    added JavaDocs to InvalidAutomataException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidAutomataException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidAutomataException.java
@@ -1,5 +1,11 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * 
+ * InvalidAutomataException
+ * 
+ * Custom exception for Invalid Automata
+ */
 public class InvalidAutomataException extends RuntimeException{
     
         public InvalidAutomataException(String message){
