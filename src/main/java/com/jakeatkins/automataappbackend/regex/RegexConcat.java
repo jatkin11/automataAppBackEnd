b4fde820ff84ae43d7ef41ae018da94173b8e457
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:27:22 +0100

    added JavaDocs to RegexConcat

--- src/main/java/com/jakeatkins/automataappbackend/regex/RegexConcat.java
+++ src/main/java/com/jakeatkins/automataappbackend/regex/RegexConcat.java
@@ -1,3 +1,15 @@
 package com.jakeatkins.automataappbackend.regex;
 
+/**
+ * 
+ * RegexConcat
+ * 
+ * Record of Concatenation RegexToken
+ * 
+ * Implements RegexToken Interface
+ * 
+ * @param left contains the left regexToken of the concatenation of a regex
+ * @param right contains the right regexToken of the concatenation of a regex
+ */
+
 public record RegexConcat (RegexToken left, RegexToken right)implements RegexToken{}
