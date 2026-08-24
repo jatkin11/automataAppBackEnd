60a55c40327f6f4cbd865ac6c2c7b7106d293768
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:59:47 +0100

    added JavaDocs to ReactFlowGraph

--- src/main/java/com/jakeatkins/automataappbackend/dto/ReactFlowGraph.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/ReactFlowGraph.java
@@ -2,4 +2,15 @@ package com.jakeatkins.automataappbackend.dto;
 
 import java.util.List;
 
+/**
+ * 
+ * ReactFlowGraph
+ * 
+ * DTO of Automaton as React Flow Graph
+ * 
+ * @param automataType Automata type used for front-end processing
+ * @param edges list of edges of an Automaton
+ * @param nodes list of edges of an Automaton
+ */
+
 public record ReactFlowGraph (String automataType, List<ReactFlowEdge> edges, List<ReactFlowNode> nodes) {}
