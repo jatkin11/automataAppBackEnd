cac0966e47f8358c0c7d93e428676ab0058b53ca
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 13:22:29 +0100

    added JavaDocs to InvalidReactFlowGraphException

--- src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidReactFlowGraphException.java
+++ src/main/java/com/jakeatkins/automataappbackend/exceptions/InvalidReactFlowGraphException.java
@@ -1,5 +1,11 @@
 package com.jakeatkins.automataappbackend.exceptions;
 
+/**
+ * 
+ * InvalidReactFlowGraphException
+ * 
+ * Custom exception for Invalid ReactFlowGraph DTO
+ */
 public class InvalidReactFlowGraphException extends RuntimeException {
     
         public InvalidReactFlowGraphException(String message){
