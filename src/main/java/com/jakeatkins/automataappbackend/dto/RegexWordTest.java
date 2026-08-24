f795bd6306771331a7360d882383feb877bee9a9
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:04:32 +0100

    added JavaDocs to RegexWordTest

--- src/main/java/com/jakeatkins/automataappbackend/dto/RegexWordTest.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/RegexWordTest.java
@@ -1,3 +1,15 @@
 package com.jakeatkins.automataappbackend.dto;
 
+/**
+ * 
+ * RegexWordTest
+ * 
+ * DTO containing a user-inputted regex string and a word to test on that string
+ * 
+ * 
+ * @param word string of user-inputted word to test
+ * @param regex string of user-inputted regex
+ */
+
+
 public record RegexWordTest(String word, String regex) {}
