2a7a5d25aae898a2d031d37d37e2351757d8a76e
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:36:11 +0100

    added JavaDocs to NFA

--- src/main/java/com/jakeatkins/automataappbackend/automata/NFA.java
+++ src/main/java/com/jakeatkins/automataappbackend/automata/NFA.java
@@ -3,6 +3,22 @@ package com.jakeatkins.automataappbackend.automata;
 import java.util.Map;
 import java.util.Set;
 
+
+/**
+ * 
+ * NFA
+ * 
+ * Represents the Back-end NFA (non-deterministic finite automaton)
+ * 
+ * Implements Automata Interface
+ * 
+ * @param startState the automatons starting state
+ * @param states the automatons set of states
+ * @param acceptingStates the automatons set of accepting states
+ * @param alphabet the automatons alphabet (set of char)
+ * @param transitionMap the transition map between source and target states within an automaton e.g. 0 - 'a' -> 1
+ * @param stateLabelMap a map of state IDs vs their label to be displayed in the front-end e.g. <0, "q0">
+ */
 public record NFA (Integer startState, 
         Set<Integer> states, 
         Set<Integer> acceptingStates, 
