bed6c45148cb30f05cc8a24d08d1c1b90ecfa845
Jake Atkins <atkins.jake@icloud.com>
2026-08-24 14:03:06 +0100

    added JavaDocs to RegexString

--- src/main/java/com/jakeatkins/automataappbackend/dto/RegexString.java
+++ src/main/java/com/jakeatkins/automataappbackend/dto/RegexString.java
@@ -1,3 +1,13 @@
 package com.jakeatkins.automataappbackend.dto;
 
+/**
+ * 
+ * RegexString
+ * 
+ * DTO of a a regex string entered by the user
+ * 
+ * @param regex string of user-inputted Regex
+ * 
+ */
+
 public record  RegexString(String regex) {}
