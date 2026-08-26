aa2cb32aaa823fdd68f9f57b9c2080c5193b64d0
Jake Atkins <atkins.jake@icloud.com>
2026-08-26 21:41:26 +0100

    refactored WordTester and added JavaDocs

--- src/main/java/com/jakeatkins/automataappbackend/algos/WordTester.java
+++ src/main/java/com/jakeatkins/automataappbackend/algos/WordTester.java
@@ -12,8 +12,34 @@ import com.jakeatkins.automataappbackend.validators.AutomataValidator;
 import com.jakeatkins.automataappbackend.validators.RegexValidator;
 import com.jakeatkins.automataappbackend.validators.WordValidator;
 
+/**
+ * WordTester
+ * 
+ * Tests a user-inputted word on a rege x or automaon
+ */
 public class WordTester {
 
+/** 
+ * Tests user-inputted word on automaton
+ * 
+ * Goes through each symbol in a word and checks whats reachable by checking the transitionMap, 
+ * and using whats reachable as the source state for the next symbol. If any final reachable state is accepting, then it returns true
+ * 
+ * Process:
+ * - validates automata and word
+ * - creates a new set of startStates and adds the starting state from passed automata
+ * - gets the epsilon closure of that set of startStates - called 'states'
+ * - then, for each symbol in the word:
+ *          for each state in the 'states':
+ *                adds any reachable state from that state on that symbol to a set of reachable states (using the transitionMap)
+ *          states is updated to the epislon closure of the reachable states, the loop moves to next symbol
+ * if any of the final reachable states are accepting states then it returns true, else false
+ * 
+ * 
+ * @param automata automaton to test
+ * @param word word to test
+ * @return boolean test result
+ */
 public static boolean testAutomata(Automata automata, String word){
 
     AutomataValidator.validate(automata);
@@ -30,11 +56,11 @@ public static boolean testAutomata(Automata automata, String word){
         Set<Integer> nextStates = new HashSet<>();
 
         for(Integer state: states){
-            Set<Integer> reachableStates = automata.transitionMap().getOrDefault(state,Collections.emptyMap()).getOrDefault(symbol,Collections.emptySet());
+            Set<Integer> reachableStates = transitionMap.getOrDefault(state,Collections.emptyMap()).getOrDefault(symbol,Collections.emptySet());
             nextStates.addAll(reachableStates);
         }
 
-        states = EpsilonClosure.epsilonClosure(nextStates, automata.transitionMap());
+        states = EpsilonClosure.epsilonClosure(nextStates, transitionMap);
     }
 
     for(Integer state : states){
@@ -45,6 +71,20 @@ public static boolean testAutomata(Automata automata, String word){
     return false;
 }
 
+/**
+ * Tests user-inputted word on regex string
+ * 
+ * Reuses RegexTokensier, RegexToNfaConverter and testAutomata function
+ * 
+ * Process:
+ * - Validates regex and word
+ * - tokenises regex
+ * - converts regexToken into NFA
+ * - uses testAutomata to test word on new NFA
+ * @param regex regex string to test
+ * @param word word to test
+ * @return boolean test-result
+ */
     public static boolean testRegexString(String regex, String word){
         RegexValidator.validate(regex);
         WordValidator.validate(word);
