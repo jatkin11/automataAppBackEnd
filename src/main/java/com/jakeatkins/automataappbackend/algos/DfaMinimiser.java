bedbcd607d22b8b534a701f28cd4622cd20d14f5
Jake Atkins <atkins.jake@icloud.com>
2026-08-28 21:05:50 +0100

    added JavaDocs to DfaMinimiser

--- src/main/java/com/jakeatkins/automataappbackend/algos/DfaMinimiser.java
+++ src/main/java/com/jakeatkins/automataappbackend/algos/DfaMinimiser.java
@@ -2,16 +2,47 @@ package com.jakeatkins.automataappbackend.algos;
 
 import com.jakeatkins.automataappbackend.automata.DFA;
 import com.jakeatkins.automataappbackend.automata.NFA;
-import com.jakeatkins.automataappbackend.validators.*;
+import com.jakeatkins.automataappbackend.validators.AutomataValidator;
 
+
+/**
+ * 
+ * DfaMinimiser
+ * 
+ * 
+ * minimises an automaton using Brzozowskis' algo
+ * 
+ */
 public class DfaMinimiser {
     
+  /**
+   * NFA-to-minimised-DFA by:
+   * - validating NFA
+   * - converting NFA-to-DFA
+   * - using brzozowskiAlgo
+   * 
+   * @param nfa NFA to convert into minimised DFA
+   * @return minimised DFA
+   */
     public static DFA minimise(NFA nfa){
       AutomataValidator.validate(nfa);
       DFA dfa = NfaToDfaConverter.convert(nfa);
       return brzozowskiAlgo(dfa);
     }
 
+
+    /**
+     * minimises DFA
+     * 
+     * Adapted from Brzozowski algorithm found in report chapter 4.4 (García, López and Vázquez de Parga, 2015)
+     * 
+     * - Reverses DFA -> Subset construction -> Reverses DFA -> subset construction
+     * 
+     * Uses DfaReverser and NfaToDfaConverter
+     * 
+     * @param dfa to minimise
+     * @return minimised DFA
+     */
     private static DFA brzozowskiAlgo(DFA dfa){
       NFA nfa1 = DfaReverser.reverse(dfa);
       DFA dfa1 = NfaToDfaConverter.convert(nfa1);
